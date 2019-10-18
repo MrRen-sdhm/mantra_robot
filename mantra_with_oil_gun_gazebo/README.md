10079c0345afc0d4b1f9036fe3fa226180627019
MrRen-sdhm <rzy.1996@qq.com>
2019-10-18 17:07:04 +0800

    add some package for mantra_with_oil_gun

---
+++ mantra_with_oil_gun_gazebo/README.md
@@ -0,0 +1,16 @@
+## mantra_with_oil_gun_gazebo
+
+
+
+启动gazebo环境：
+
+```
+roslaunch mantra_with_oil_gun_gazebo mantra_with_oil_gun_gazebo_control.launch
+```
+
+启动gazebo和moveit：
+
+```
+roslaunch mantra_with_oil_gun_gazebo mantra_with_oil_gun_moveit_gazebo.launch
+```
+
--- mantra_with_oil_gun_moveit_config/scripts/abutment.py
+++ mantra_with_oil_gun_moveit_config/scripts/abutment.py
@@ -3,6 +3,7 @@
 
 import sys
 import copy
+import time
 import rospy
 import tf
 import moveit_commander
@@ -55,8 +56,8 @@ class MoveGroupPythonIntefaceTutorial(object):
     ## Instantiate a `MoveGroupCommander`_ object.
     group_name = "arm"
     group = moveit_commander.MoveGroupCommander(group_name)
-    # group.set_max_velocity_scaling_factor(0.25)
-    # group.set_max_acceleration_scaling_factor(0.5)
+    group.set_max_velocity_scaling_factor(0.5)
+    group.set_max_acceleration_scaling_factor(0.2)
 
     # 当运动规划失败后，允许重新规划
     # group.allow_replanning(True)
@@ -290,12 +291,12 @@ def main():
     tutorial.go_to_pose_named("home")
 
     tutorial.go_to_pose_named("point_1")
-    # tutorial.go_to_pose_named("point_2")
-    tutorial.go_to_pose_named("point_3")
+    tutorial.go_to_pose_named("point_7")
+    tutorial.go_to_pose_named("point_8")
+    time.sleep(1)
     tutorial.go_to_pose_named("point_4")
-    tutorial.go_to_pose_named("point_5")
     tutorial.go_to_pose_named("point_6")
-    rospy.sleep(1)
+    time.sleep(2)
     tutorial.go_to_pose_named("point_4")
     tutorial.go_to_pose_named("point_1")
 
